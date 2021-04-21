a5384bc7f847ab08309757a3507dfc30c2d161f5
SeanPattinson <48321498+SeanPattinson@users.noreply.github.com>
2021-04-21 13:34:03 +0100

    Token System Added
    Added Code and components for token system in its first iteration

---
+++ src/ChatRoom/components/tokens.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+
+
+    class Tokens extends React.Component {
+        state = {
+            data: [],
+            tokens: [],
+            roomId: this.props.roomId,
+        }
+
+        getNewCoords = (e) => {
+            e.preventDefault();
+            var posX = 0;
+            var posY = 0;
+            var cssMatrix = e.target.style.transform;
+            console.log(cssMatrix);
+            cssMatrix = cssMatrix
+                .split('(')[1]
+                .split(')')[0]
+                .split(',').map(parseFloat);
+            posX = cssMatrix[0];
+            posY = cssMatrix[1];
+            var coords = {'img-src':e.target.src,'x':posX, 'y':posY};
+            this.props.tokenCallback(coords);
+        }
+
+        render() {
+
+
+            return (
+
+                <div>
+                    <img src='http://simpleicon.com/wp-content/uploads/plus-256x256.png'/>
+                    {/*This needs moving to token component to make each token draggable*/}
+                    <Draggable
+                        onStart={this.handleDragStart}
+                        onDrag={this.handleDrag}
+                        onStop={this.getNewCoords}>
+                        <img src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Folliebarder%2Ffiles%2F2018%2F03%2Fsuper_mario_plumber_new-1200x675.jpg' id='token1'/>
+                    </Draggable>
+
+                    <Draggable
+                        onStart={this.handleDragStart}
+                        onDrag={this.handleDrag}
+                        onStop={this.getNewCoords}>
+                        <img src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Folliebarder%2Ffiles%2F2018%2F03%2Fsuper_mario_plumber_new-1200x675.jpg' id='token2'/>
+                    </Draggable>
+                </div>
+
+
+            );
+
+
+        }
+    }
+    export default Tokens;
+
+
