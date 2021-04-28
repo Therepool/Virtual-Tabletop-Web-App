d0988323ae1dcea593fcd8d6800b24c90443c3e0
Matthew Woolley <matthewwoolley31@gmail.com>
2021-04-28 12:28:37 +0100

    Stretch map image

--- src/ChatRoom/components/map.js
+++ src/ChatRoom/components/map.js
@@ -12,7 +12,7 @@ class Map extends React.Component {
     console.log("AAAA");
     let map = document.getElementsByClassName("map")[0];
     map.style.backgroundImage = "url(https://www.freeiconspng.com/thumbs/grid-png/grid-png-transparent-pic-18.png), url('http://unn-w17016042.newnumyspace.co.uk/test/maps/" + this.state.mapName;
-    map.style.backgroundSize = "500px 500px, 500px";
+    map.style.backgroundSize = "contain, contain";
     map.style.backgroundRepeat = "repeat, no-repeat";
     map.style.backgroundPosition = "center center, center center"
   }
@@ -20,7 +20,7 @@ class Map extends React.Component {
     render() {
         return (
             <div onClick={this.handleClick} class="mapThumb">
-              
+
                 <img src={'http://unn-w17016042.newnumyspace.co.uk/test/maps/' /*+ this.roomId + '/' */ + this.state.mapName}/>
             </div>
         );
