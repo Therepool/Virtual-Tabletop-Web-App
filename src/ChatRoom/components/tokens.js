7ad530c9848484346bcd84b4c4d75a7debaab367
SeanPattinson <48321498+SeanPattinson@users.noreply.github.com>
2021-04-29 13:15:52 +0100

    updated token subsytem and implemented upload modal
    updated token subsytem and implemented upload modal

--- src/ChatRoom/components/tokens.js
+++ src/ChatRoom/components/tokens.js
@@ -1,25 +1,101 @@
 import React from 'react';
 import Token from './token.js';
+import Modal from './modal.js';
+
 
     class Tokens extends React.Component {
         state = {
             data: [],
             tokens: [],
             roomId: this.props.roomId,
+            show: false,
         }
+
+        componentDidMount() {
+            let array = [];
+            const basepath = "http://localhost/vtapp/tabletop";
+            const url = "/src/ChatRoom/components/get_tokens.php";
+            fetch(basepath + url)
+                .then( (response) => response.json() )
+                .then( (data) => {
+                    for (var i = 0; i < data.length; i++) {
+                    let details = {};
+                    details.id = data[i];
+                    details.x = 0;
+                    details.y = 0;
+                    details.src = basepath + '/tokens/' + data[i];
+                    array.push(details);
+                    console.log(array);
+                }
+                    this.setState({data:array})
+                })
+                .catch ((err) => {
+                        console.log("something went wrong ", err)
+                    }
+                );
+        }
+
+        componentDidUpdate(prevProps) {
+            if (this.props.tokens.length > prevProps.tokens.length) {
+                this.setState({data: this.props.tokens});
+            }
+        }
+
 
         lobbyCallback = (childData) => {
             this.props.tokenCallback(childData);
         }
 
+        showModal = () => {
+            this.setState({ show: true });
+            let offsets = document.getElementById('upload-token').getBoundingClientRect();
+            console.log(offsets.top, offsets.left);
+        };
+
+        hideModal = () => {
+            this.setState({ show: false });
+        };
+
+        handleUpload = () => {
+            const basepath = "http://localhost/vtapp/tabletop";
+            const url = "/src/ChatRoom/components/upload_tokens.php";
+            let submit_button = document.getElementById('token-upload');
+            let formData = new FormData();
+            formData.append("file", document.getElementById("token-file").files[0]);
+            submit_button.disabled = true;
+            fetch(basepath + url, {
+                method: 'POST',
+                body: formData
+            }).then(function(data) {
+                return data.text();
+            }).then(function(response) {
+                    submit_button.disabled  = false;
+                    if (response == "Error") {
+                        alert("There was an error uploading the token, please try again");
+                    } else {
+                        document.getElementById("token-file").value = null;
+                    }
+                }
+            );
+        }
+
+
         render() {
 
+            this.showModal = this.showModal.bind(this);
+            this.hideModal = this.hideModal.bind(this);
+            this.handleUpload = this.handleUpload.bind(this);
 
             return (
 
-                <div>
-                    <img src='http://simpleicon.com/wp-content/uploads/plus-256x256.png'/>
-                    <Token src={'http://simpleicon.com/wp-content/uploads/plus-256x256.png'} tokensCallback={this.lobbyCallback}/>
+                <div className={'token-container'}>
+                    <h2>Tokens</h2>
+                    <Modal show={this.state.show} handleClose={this.hideModal} handleUpload={this.handleUpload}/>
+                    <img src='http://simpleicon.com/wp-content/uploads/plus-256x256.png' id='upload-token' onClick={this.showModal}/>
+                    {
+                        this.state.data.map( (details, i)  => (<Token Key={i} details={details} roomId={this.props.roomId} tokensCallback={this.lobbyCallback}/> ) )
+                    }
+
                 </div>
 
 
@@ -27,57 +103,7 @@ import Token from './token.js';
 
 
         }
-
-class Tokens extends React.Component {
-    state = {
-        data: [],
-        tokens: [],
-        roomId: this.props.roomId,
     }
-
-    getNewCoords = (e) => {
-        e.preventDefault();
-        var posX = 0;
-        var posY = 0;
-        var cssMatrix = e.target.style.transform;
-        console.log(cssMatrix);
-        cssMatrix = cssMatrix
-            .split('(')[1]
-            .split(')')[0]
-            .split(',').map(parseFloat);
-        posX = cssMatrix[0];
-        posY = cssMatrix[1];
-        var coords = {'img-src':e.target.src,'x':posX, 'y':posY};
-        this.props.tokenCallback(coords);
-    }
-
-    render() {
+    export default Tokens;
 
 
-        return (
-
-            <div>
-                <img src='http://simpleicon.com/wp-content/uploads/plus-256x256.png'/>
-                {/*This needs moving to token component to make each token draggable*/}
-                <Draggable
-                    onStart={this.handleDragStart}
-                    onDrag={this.handleDrag}
-                    onStop={this.getNewCoords}>
-                    <img src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Folliebarder%2Ffiles%2F2018%2F03%2Fsuper_mario_plumber_new-1200x675.jpg' id='token1'/>
-                </Draggable>
-
-                <Draggable
-                    onStart={this.handleDragStart}
-                    onDrag={this.handleDrag}
-                    onStop={this.getNewCoords}>
-                    <img src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Folliebarder%2Ffiles%2F2018%2F03%2Fsuper_mario_plumber_new-1200x675.jpg' id='token2'/>
-                </Draggable>
-            </div>
-
-
-        );
-
-
-    }
-}
-export default Tokens;
